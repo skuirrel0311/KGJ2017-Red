ecc443a853d71ead07c2806d0c194e64238ff132
NAOTO <suniwocarina@gmail.com>
2017-08-10 17:37:18 +0900

    サウンド関係を実装

---
+++ KGJ2017-Red/Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -0,0 +1,39 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Audiokinetic Wwise generated include file. Do not edit.
+//
+/////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#ifndef __WWISE_IDS_H__
+#define __WWISE_IDS_H__
+
+#include <AK/SoundEngine/Common/AkTypes.h>
+
+namespace AK
+{
+    namespace EVENTS
+    {
+        static const AkUniqueID BGM_MAIN = 2633060887U;
+        static const AkUniqueID BGM_TITLE = 737535402U;
+        static const AkUniqueID DAMAGE = 1786804762U;
+        static const AkUniqueID FOOTSTEPS = 2385628198U;
+        static const AkUniqueID GAMEOVER = 4158285989U;
+        static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID RESULT = 936499202U;
+    } // namespace EVENTS
+
+    namespace BANKS
+    {
+        static const AkUniqueID INIT = 1355168291U;
+        static const AkUniqueID KGJ2017 = 1768708089U;
+    } // namespace BANKS
+
+    namespace BUSSES
+    {
+        static const AkUniqueID MASTER_AUDIO_BUS = 3803692087U;
+        static const AkUniqueID MASTER_SECONDARY_BUS = 805203703U;
+    } // namespace BUSSES
+
+}// namespace AK
+
+#endif // __WWISE_IDS_H__
